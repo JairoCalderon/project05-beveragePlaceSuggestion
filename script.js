ae9f771e62f4a77d602cba554d32e10c6999fd98
JairoCalderon <j.calderoneng@gmail.com>
2022-07-21 16:15:56 -0400

    initializing project 5 - Beverage Place suggestion

---
+++ script.js
@@ -0,0 +1,84 @@
+const drinks = {
+  coffee: [
+    {
+      title: 'Early Bird',
+      price: '$$',
+    },
+    {
+      title: 'Strange Love',
+      price: '$$',
+    },
+    {
+      title: 'Fahrenheit',
+      price: '$$',
+    },
+    {
+      title: 'Voodoo Child',
+      price: '$$',
+    },
+    {
+      title: 'Dark Horse',
+      price: '$',
+    },
+    {
+      title: 'Cops',
+      price: '$',
+    },
+    {
+      title: 'i deal',
+      price: '$',
+    },
+    {
+      title: 'Jimmy\'s',
+      price: '$',
+    }
+  ],
+  tea: [
+    {
+      title: 'Icha',
+      price: '$$',
+    },
+    {
+      title: 'Ten Ren\'s',
+      price: '$$',
+    },
+    {
+      title: "David's Tea",
+      price: '$$',
+    },
+    {
+      title: 'Bubble Lee',
+      price: '$$',
+    },
+    {
+      title: 'Chattime',
+      price: '$',
+    },
+    {
+      title: 'Tealish',
+      price: '$',
+    },
+    {
+      title: 'Kung Fu',
+      price: '$',
+    },
+    {
+      title: 'Gong Cha',
+      price: '$',
+    }
+  ]
+};
+
+
+// Our code goes here:
+
+//get data from the user
+//----query select type of drink
+//----query select value of drink
+//----store those values in variables
+//according to the values obtained from user, iterate the drink object
+//ramdonly select a drink accordingly to user preferences
+//display the result
+//----create a new html paragraph to append the location name of the shop
+
+
